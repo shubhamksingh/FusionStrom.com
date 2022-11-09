3fad4ce029c6ac7365e5a8787c01ac5120f6c60e
Kundan Kumar Keshri <101567147+kundan799@users.noreply.github.com>
2022-11-09 10:57:55 +0530

    kundan_day2

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,23 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import MenPage from './components/ProductPage/MenPage';
+import MensAccordion from './components/ProductPage/MensAccordion';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div >
+      <MenPage />
     </div>
   );
 }
