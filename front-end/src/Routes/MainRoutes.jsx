13d1b9d6969f33563e6c8b9b61e6828b051f2c0d
Omgupta12 <rahulgupta1997242@gmail.com>
2022-11-13 01:46:19 +0530

    adminlogin page,signin & sigin page responsiveness

--- front-end/src/Routes/MainRoutes.jsx
+++ front-end/src/Routes/MainRoutes.jsx
@@ -1,63 +1,36 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
-
-
 import Navbar from "../components/Navbar/navbar";
 import Footer from "../components/Footer/Footer";
 import ProductView from "../components/ProductView/ProductView";
-
-
 import Dashboard from "../components/dashboard/dashboard";
-
-
-// import Navbar2 from "../components/Navbar/Navbar2";
-//import Navbar from "../components/Navbar/navbar";
-//import Footer from "../components/Footer/Footer";
-//import ProductView from "../components/ProductView/ProductView";
-// import dashboard from "../components/dashboard/dashboard";
-//import Dashboard from "../components/dashboard/dashboard";
-
-//import Navbar2 from "../components/Navbar/Navbar2";
-//import Footer from "../components/Footer/Footer";
-//import ProductView from "../components/ProductView/ProductView";
 import CartPage from "../components/Cart/cart";
-
 import MenPage from "../components/ProductPage/MenPage";
 import Signin from "../components/Auth/Signin/Signin"
 import Signup from "../components/Auth/Signup/Signup"
 import Profile from "../components/Auth/Profile/Profile"
-
+import Checkout from "../components/Checkout/Checkout"
+import Flash from "../components/Flash/Flash";
+import Adminlogin from "../components/Auth/AdminLogin/Adminlogin";
 
 
 const MainRoutes = () => {
   return (
     <>
-
       <Navbar/>
-
-      
-
-      <br/>
-
-
       {/* <Navbar2 /> */}
 
       <Routes>
         <Route path="/" element={<Dashboard/>} />
-        <Route path="/checkout" element={<h3>Checkout</h3>} />
-        <Route path="payment" element={<h3>Payment </h3>} />
-        //<Route path="signin" element={<h3>Singin</h3>} />
-        //<Route path="createacc" element={<h3>Createacc</h3>} />
 
-    
-      {/* <Navbar2 /> */}
-    
-     // <Routes>
-        //<Route path="/" element={<h3></h3>} />
+        <Route path="payment" element={<h3>Payment </h3>} />
+
         <Route path="/signin" element={<Signin/>} />
         <Route path="/signup" element={<Signup/>} />
         <Route path="/profile" element={<Profile/>} />
+        <Route path="/checkout" element={<Checkout/>} />
         <Route path="mens" element={<MenPage/>} />
+        <Route path="/adminlogin" element={<Adminlogin/>} />
 
         <Route
           path="cart"
@@ -110,10 +83,10 @@ const MainRoutes = () => {
         <Route path="gift" element={<h3>Gift</h3>} />
         <Route path="products/:id" element={<ProductView/>} />
         <Route path="flash/women" element={<Flash />} />
-        <Route path="flash/kids" element={<Kids />} />
+        {/* <Route path="flash/kids" element={<Kids />} />
         <Route path="flash/mens" element={<Mens />} />
         <Route path="flash/home" element={<Home />} />
-        <Route path="flash/beauty" element={<Beauty />} />
+        <Route path="flash/beauty" element={<Beauty />} /> */}
 
       </Routes>
   
--- front-end/src/components/Auth/Signup/Signup.jsx
+++ front-end/src/components/Auth/Signup/Signup.jsx
@@ -1,16 +1,11 @@
 import React, { useState } from 'react'
 import { Button, Flex, Text, Stack, Center, Input, Box, Link } from '@chakra-ui/react'
-import { useNavigate } from 'react-router-dom'
-
+import { useNavigate } from "react-router-dom"
+import axios from "axios"
 
 const Signup = () => {
   const navigate = useNavigate()
 
-  const handleSignup = () => {
-    navigate("/signin")
-   
-     }
-     
   const [user, Setuser] = useState({
     first_name: "",
     last_name: "",
@@ -27,12 +22,29 @@ const Signup = () => {
     })
   }
 
- 
+  const register=()=>{
+    const {first_name,last_name,email,password} = user
+
+    if(first_name && last_name && email && password ){
+      axios.post("http://localhost:8080/signup", user)
+      .then(res=>{
+        // console(res)
+        alert("Registation Successfull")
+       navigate("/signin")
+      }
+        )
+    }
+    else{
+      alert("Fill all inputs")
+    }
+  }
+
+
 
   return (
-    <Center mt={"8rem"}>
+    <Center >
 
-      <Box style={{ display: "flex", flexDirection: "column", width: "fit-content" }}>
+      <Box mt={"1.5rem"} w={"fit-content"} >
 
         <Stack>
           <Text fontSize='1.6rem'>Create Account</Text>
@@ -42,10 +54,10 @@ const Signup = () => {
         </Stack>
 
 
-        <Box mt={5} style={{ display: "flex", flexDirection: "column" }}>
+        <Box w={["300px", "400px", "500px"]} mt={5} style={{ display: "flex", flexDirection: "column" }}>
 
           <Text fontSize='0.8rem'>Email*</Text>
-          <Input style={{ border: "1px solid black" }} w={"400px"} mb={5} type="email"
+          <Input style={{ border: "1px solid black" }} w={["300px", "300px", "400px"]} mb={5} type="email"
             placeholder='Email'
             name='email'
             value={user.email}
@@ -53,7 +65,7 @@ const Signup = () => {
           />
 
           <Text fontSize='0.8rem'>First name*</Text>
-          <Input style={{ border: "1px solid black" }} w={"400px"} mb={5} type="text"
+          <Input style={{ border: "1px solid black" }} w={["300px", "300px", "400px"]} mb={5} type="text"
             placeholder='First name'
             name='first_name'
             value={user.first_name}
@@ -62,7 +74,7 @@ const Signup = () => {
 
 
           <Text fontSize='0.8rem'>Last name*</Text>
-          <Input style={{ border: "1px solid black" }} w={"400px"} mb={5} type="text"
+          <Input style={{ border: "1px solid black" }} w={["300px", "300px", "400px"]} mb={5} type="text"
             placeholder='Last name'
             name='last_name'
             value={user.last_name}
@@ -71,7 +83,7 @@ const Signup = () => {
 
 
           <Text fontSize='0.8rem'>Create password*</Text>
-          <Input style={{ border: "1px solid black" }} w={"400px"} mb={5} type="password"
+          <Input style={{ border: "1px solid black" }} w={["300px", "300px", "400px"]} mb={5} type="password"
             placeholder='Password'
             name='password'
             onChange={handleChange}
@@ -81,13 +93,13 @@ const Signup = () => {
         <Box mt={4}>
           <input type="checkbox" />
           <Text mb={2} fontSize='0.8rem'>Keep me signed in</Text>
-          <Text w={"400px"} mb={2} fontSize='0.8rem'>You won't need to sign in as often. Only do this if you're on your personal device. Just sign out if you change your mind.</Text>
+          <Text w={["300px", "300px", "400px"]} mb={2} fontSize='0.8rem'>You won't need to sign in as often. Only do this if you're on your personal device. Just sign out if you change your mind.</Text>
         </Box>
 
 
-        <Text w={"400px"} mb={5} fontSize='0.8rem'>By tapping Next, you agree to our <Text as={"u"}><Link href=''>Privacy Policy </Link></Text> and <Text as={"u"}><Link href=''>Terms & Conditions.</Link></Text></Text>
+        <Text w={["300px", "300px", "400px"]} mb={5} fontSize='0.8rem'>By tapping Next, you agree to our <Text as={"u"}><Link href=''>Privacy Policy </Link></Text> and <Text as={"u"}><Link href=''>Terms & Conditions.</Link></Text></Text>
 
-        <Button bg={"black"} color={"white"} w={"400px"} onClick={handleSignup}>Create Account</Button>
+        <Button bg={"black"} color={"white"} w={["300px", "300px", "400px"]} onClick={register}>Create Account</Button>
 
 
       </Box>
