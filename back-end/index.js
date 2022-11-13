054d1f8c9fff1bdb76cadb4024cca90fbfa3f2bd
Janaki Kangalgouda <42041306+janakigouda@users.noreply.github.com>
2022-11-13 19:27:57 +0530

    edits

--- back-end/index.js
+++ back-end/index.js
@@ -43,8 +43,8 @@ app.get('/', (req, res) => {
 
 
 
-// mongoose.connect(process.env.MONGO_URL)
-mongoose.connect("mongodb://localhost:27017/fusionstrom")
+mongoose.connect(process.env.MONGO_URL)
+// mongoose.connect("mongodb://localhost:27017/fusionstrom")
 .then(()=> console.log('DB connection successfull !'))
 .catch((err)=> {
   console.log(err);
