d258296a0853784f22af636ebe4a1fa23057cb6d
Janaki Kangalgouda <42041306+janakigouda@users.noreply.github.com>
2022-11-13 19:01:16 +0530

    edits

--- back-end/index.js
+++ back-end/index.js
@@ -13,7 +13,8 @@ const wishlistRoute = require('./routes/wishlist')
 dotenv.config();
 
 const app = express();
-
+var cors = require('cors');
+app.use(cors());
 
 
 
@@ -42,7 +43,8 @@ app.get('/', (req, res) => {
 
 
 
-mongoose.connect(process.env.MONGO_URL)
+// mongoose.connect(process.env.MONGO_URL)
+mongoose.connect("mongodb://localhost:27017/fusionstrom")
 .then(()=> console.log('DB connection successfull !'))
 .catch((err)=> {
   console.log(err);
